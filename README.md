f840999f999c9a6f308981963ed505d588e38c66
botirbek.kulzhanov <botirbek.kulzhanov@fabrique.studio>
2022-12-06 18:06:05 +0600

    Added some test cases

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Automated tests written in Java+Selenide for OrangeHRM
 
 For reporting slf4j-simple is used.
+
+Test cases are in [google.sheets](https://docs.google.com/spreadsheets/d/13C_b8cag9D6BHmjuHhyrnjSGicsv6GPY3jH6rGkVYyg/edit?usp=sharing). Please, send me request to access the document.
--- src/test/java/orangeHrm/tests/LoginTest.java
+++ src/test/java/orangeHrm/tests/LoginTest.java
@@ -20,7 +20,7 @@ public class LoginTest extends BaseTest
     @Test
     @Order(1)
     @DisplayName("Wrong password and right username")
-    public void wrongPassword()
+    public void userCanNotLogWithWrongPwd()
     {
         LoginPage.resetPwdLink.shouldBe(visible);
         LoginPage.orangeHRMLogo.shouldBe(visible);
@@ -30,8 +30,30 @@ public class LoginTest extends BaseTest
 
     @Test
     @Order(2)
-    @DisplayName("Right password and right username")
-    public void rightPassword()
+    @DisplayName("Right password and wrong username")
+    public void userCanNotLogWithWrongUserName()
+    {
+        LoginPage.resetPwdLink.shouldBe(visible);
+        LoginPage.orangeHRMLogo.shouldBe(visible);
+        LoginPage.login("Admin","admin1");
+        $(By.xpath("//div[@class='oxd-pie-chart']")).shouldNotBe(visible);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Wrong password and username")
+    public void userCanNotLogWithWrongCredentials()
+    {
+        LoginPage.resetPwdLink.shouldBe(visible);
+        LoginPage.orangeHRMLogo.shouldBe(visible);
+        LoginPage.login("Admin","admin1");
+        $(By.xpath("//div[@class='oxd-pie-chart']")).shouldNotBe(visible);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Right username and password\n")
+    public void userCanLogin()
     {
         LoginPage.resetPwdLink.shouldBe(visible);
         LoginPage.orangeHRMLogo.shouldBe(visible);
